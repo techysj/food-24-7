eae9390ad6993d7f0b628c12f07c24787e70b390
techysj <techysj0505@gmail.com>
2023-11-24 22:46:48 +0530

    Code splitting for about page fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Error from "./components/Error";
 import RestroMenu from "./components/RestroMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
@@ -10,7 +9,7 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
-
+const About = lazy(() => import("./components/About"));
 const AppLayout = () => {
   return (
     <Provider store={appStore}>
